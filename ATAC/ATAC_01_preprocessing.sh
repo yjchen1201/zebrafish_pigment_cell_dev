bae14653e9f9c16c491efea717b4bc2e59b476c1
yujiechen <37003579+yjchen1201@users.noreply.github.com>
2021-09-08 12:41:39 -0500

    Update ATAC_01_preprocessing.sh

--- ATAC/ATAC_01_preprocessing.sh
+++ ATAC/ATAC_01_preprocessing.sh
@@ -3,6 +3,8 @@
 #- samtools version 1.3.1: Used to sort and downsample bam files for downstream processing
 #- picard v2.8.1: Used to mark and remove duplicate reads
 #- methylQA v0.1.6: Use to covert bam to single-end read bed file
+#- kentUCSC tools: Use bigWigMerge/bedGraphToBigWig to convert bigwig to bedgraph or vice versa
+
 
 # Adapter trimming
 ## Input SAMPLE_R1.fastq.gz, SAMPLE_R2.fastq.gz. 
@@ -61,4 +63,25 @@ methylQA density -Q 10 -r -T -E 0 -I 2000 -o "SAMPLE_MAPQ30.sorted.rmdup.downsam
 awk -v OFS="\t" '{if ($6=="+") print $1,$2+4,$3+4,$4,$5,$6; else if ($6=="-") print $1,$2-5,$3-5,$4,$5,$6}' "SAMPLE_MAPQ30.sorted.rmdup.downsampled.extend.bed" > "SAMPLE_MAPQ30.sorted.rmdup.downsampled.Tshift.bed"
 
 #NOTE THAT THIS CAN LEAD TO NEGATIVE VALUES FOR READS AT ENDS OF CHROMOSOMES, so make sure the start site is >0
-```
+
+
+## Generate ATAC signal bigwig files ## [For Fig.3d and SupFig.7a]
+## The "*downsampled.bigwig" files are the outputs from "methylQA density" step
+# Merge ATAC signals from each replicates using UCSC tool bigWigMerge
+bigWigMerge 15somite_pos_Rep1.downsampled.bigWig 15somite_pos_Rep2.downsampled.bigWig ATAC_s15pos_merged_forDeeptools.bedGraph
+bigWigMerge 24hpf_pos_Rep1.downsampled.bigWig 24hpf_pos_Rep2.downsampled.bigWig ATAC_s24pos_merged_forDeeptools.bedGraph
+bigWigMerge Mel_Rep1.downsampled.bigWig Mel_Rep2.downsampled.bigWig ATAC_Mel_merged_forDeeptools.bedGraph
+bigWigMerge Iri_Rep1.downsampled.bigWig Iri_Rep2.downsampled.bigWig ATAC_Iri_merged_forDeeptools.bedGraph
+
+# Sort bedgraph files
+sort -k1,1 -k2,2n ATAC_s15pos_merged_forDeeptools.bedGraph > ATAC_s15pos_merged_forDeeptools.sorted.bedGraph
+sort -k1,1 -k2,2n ATAC_s24pos_merged_forDeeptools.bedGraph > ATAC_s24pos_merged_forDeeptools.sorted.bedGraph
+sort -k1,1 -k2,2n ATAC_Mel_merged_forDeeptools.bedGraph > ATAC_Mel_merged_forDeeptools.sorted.bedGraph
+sort -k1,1 -k2,2n ATAC_Iri_merged_forDeeptools.bedGraph > ATAC_Iri_merged_forDeeptools.sorted.bedGraph
+
+# Convert sorted bedGraph file to bigWig file
+## danRer10.chrom.sizes is in "Input_files" folder
+bedGraphToBigWig ATAC_s15pos_merged_forDeeptools.sorted.bedGraph danRer10.chrom.sizes ATAC_s15pos_merged_forDeeptools.bw
+bedGraphToBigWig ATAC_s24pos_merged_forDeeptools.sorted.bedGraph danRer10.chrom.sizes ATAC_s24pos_merged_forDeeptools.bw
+bedGraphToBigWig ATAC_Mel_merged_forDeeptools.sorted.bedGraph danRer10.chrom.sizes ATAC_Mel_merged_forDeeptools.bw
+bedGraphToBigWig ATAC_Iri_merged_forDeeptools.sorted.bedGraph danRer10.chrom.sizes ATAC_Iri_merged_forDeeptools.bw
